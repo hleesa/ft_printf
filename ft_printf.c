045d14a568778bc7b7ce6d6e9ac60a0fa7125155
Sanghoon Lee <salee2@c5r1s2.42seoul.kr>
2022-07-29 20:07:31 +0900

    ft_printf begin

--- ft_printf.c
+++ ft_printf.c
@@ -10,7 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
-{
-}
+#include "ft_printf.h"
 
+//int	ft_printf(const char *, ...)
+//{
+//
+//}
+
+
+#include <stdio.h>
+void printNumber(int args, ...)
+{
+	va_list ap;
+
+	va_start(ap, args);
+	for (int i = 0; i < args; ++i)
+	{
+		int num = va_arg(ap, int);
+		printf("%d ", num);
+	}
+	va_end(ap);
+
+	printf("\n");
+}
+int main()
+{
+	printNumber(1, 10);
+	printNumber(2, 10, 20);
+	printNumber(3, 10, 20, 30);
+	printNumber(4, 10, 20, 30, 40);
+
+	return 0;
+}
