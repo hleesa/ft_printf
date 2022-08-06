c0ebfe5d752acbdb1981ad950a2986453b6b0799
Sanghoon Lee <salee2@c3r2s5.42seoul.kr>
2022-08-06 14:36:33 +0900

    norm edit

--- ft_printf_manda_func_three.c
+++ ft_printf_manda_func_three.c
@@ -1,16 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_manda_func_two.c                         :+:      :+:    :+:   */
+/*   ft_printf_manda_func_three.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salee2 <salee2@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 16:34:02 by salee2            #+#    #+#             */
-/*   Updated: 2022/08/06 11:07:07 by salee2           ###   ########.fr       */
+/*   Updated: 2022/08/06 14:35:53 by salee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+size_t	get_size(int n)
+{
+	size_t	size;
+
+	size = 0;
+	if (n <= 0)
+		++size;
+	while (n)
+	{
+		++size;
+		n /= 10;
+	}
+	return (size);
+}
+
+void	to_string(int n, char *str, size_t idx, size_t len)
+{
+	int	sign;
+
+	sign = 1;
+	if (n < 0)
+	{
+		sign = -1;
+		str[0] = '-';
+	}
+	if (-10 < n && n < 10)
+	{
+		str[idx] = n * sign + '0';
+		return ;
+	}
+	to_string(n / 10 * sign, str, idx - 1, len);
+	str[idx] = n % 10 * sign + '0';
+	return ;
+}
 
 char	*ft_utoa(unsigned int n)
 {
